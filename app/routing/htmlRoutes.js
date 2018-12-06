ca5b80969a583088a294b3a3883ff2d57f24d239
Pandey <vaibhav.pandey@wmeng.com>
2018-12-05 18:21:40 -0800

    test code

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 module.exports = function(app){
     // routing
-    app.get('*',(req,res)=>{
+    app.get('/home',(req,res)=>{
         res.sendFile(path.join(__dirname,'..','/public/home.html'));
     });
-    app.get('/survey',(req,res)=>{
+    app.get('*',(req,res)=>{
         res.sendFile(path.join(__dirname,'..','/public/survey.html'));
     });
 }
